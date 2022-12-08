cef79d5439f9c9b3d64d71665e214a24d70bdad5
Edison Matias <devedisonmatias@gmail.com>
2022-12-08 09:35:26 -0300

    added tab panels to home page

--- src/pages/CriteriosPage.vue
+++ src/pages/CriteriosPage.vue
@@ -1,37 +1,9 @@
 <template>
-  <q-page class="q-pa-md">
-    <q-header>
-      <q-toolbar>
-        <q-btn dense rounded flat icon="arrow_back" to="/home" />
-        <q-toolbar-title>
-          Critérios
-        </q-toolbar-title>
-      </q-toolbar>
-    </q-header>
-
-    <q-select v-model="ambiente" outlined class="q-mb-md" label="Selecione o ambiente" behavior="menu" :options="ambientes" />
-
-    <q-card class="q-mb-md" v-bind:key="index" v-for="(criterio, index) in criterios">
-      <q-card-section>
-        <div class="text-bold">{{ criterio.nome }}</div>
-      </q-card-section>
-      <q-list separator>
-        <q-item v-bind:key="index" v-for="(subcriterio, index) in criterio.subcriterios">
-          <q-item-section>
-            <q-item-label lines="1">{{ subcriterio.nome }}</q-item-label>
-          </q-item-section>
-          <q-item-section side>
-            <q-btn outline :href="subcriterio.link" style="width: 128px" :color="subcriterio.respondido ? 'grey' : 'primary'" :label="subcriterio.respondido ? 'Respondido' : 'Responder'" />
-          </q-item-section>
-        </q-item>
-      </q-list>
-    </q-card>
-  </q-page>
+  <q-select outlined label="Selecione o ambiente" behavior="menu" :options="ambientes" />
 </template>
 
 <script>
 import { defineComponent } from 'vue';
-import { api } from '../boot/axios';
 
 const example = ['Ambiente 1', 'Ambiente 2'];
 
@@ -39,14 +11,8 @@ export default defineComponent({
   name: 'CriteriosPage',
   data() {
     return {
-      criterios: [],
-      ambiente: '',
       ambientes: example,
     };
-  },
-  async mounted() {
-    const { data } = await api.get('/criterio');
-    this.criterios = data.body;
   },
 });
 </script>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,69 +1,66 @@
 <template>
-  <q-header>
-    <q-toolbar class="justify-between" />
-  </q-header>
+  <div class="q-ma-md">
+    <q-card>
+      <q-card-section class="flex justify-between q-py-lg">
+        <q-circular-progress show-value size="75px" color="primary" track-color="grey-3"
+          :value="calcularProgressoTotal">
+          {{ calcularProgressoTotal }}%
+        </q-circular-progress>
 
-  <q-card clickable @click="goToCriterios()" v-ripple class="q-ma-md">
-    <q-card-section class="flex justify-between q-py-lg">
-      <q-circular-progress
-        show-value
-        size="75px"
-        color="primary"
-        track-color="grey-3"
-        :value="calcularProgressoTotal"
-      >
-        {{ calcularProgressoTotal }}%
-      </q-circular-progress>
-      <div>
-        <div align="right" class="text-bold">{{ totalCriteriosCompletos }} de 21 critérios concluídos</div>
-        <div align="right">Clique para responder</div>
-      </div>
-    </q-card-section>
-  </q-card>
+        <div align="right" style="width: 70%">
+          <div class="text-bold">{{ totalCriteriosCompletos }} de 21 critérios concluídos</div>
+          Vamos realizar a avaliação da sua unidade
+        </div>
 
-  <q-fab class="fab" color="primary" direction="up">
-    <q-fab-action
-    label="Ambiente"
-    to="/cadenviroments"
-    external-label
-    label-position="left"
-    icon="chair"
-  />
-  <q-fab-action
-    label="Unidade"
-    to="/unidade"
-    external-label
-    label-position="left"
-    icon="home"
-  />
-  </q-fab>
+      </q-card-section>
+
+      <q-tabs v-model="tab" class="text-grey" active-color="primary" indicator-color="primary" align="justify"
+        narrow-indicator>
+        <q-tab name="geral" label="Aspectos Gerais" />
+        <q-tab name="ambientes" label="Ambientes" />
+        <q-tab name="projeto" label="Projeto" />
+        <q-tab name="gestao" label="Gestão" />
+      </q-tabs>
+
+      <q-tab-panels v-model="tab" animated>
+        <q-tab-panel name="geral">
+          <CriteriosPage />
+        </q-tab-panel>
+        <q-tab-panel name="ambientes">
+          <CriteriosPage />
+        </q-tab-panel>
+        <q-tab-panel name="projeto">
+          <CriteriosPage />
+        </q-tab-panel>
+        <q-tab-panel name="gestao">
+          <CriteriosPage />
+        </q-tab-panel>
+      </q-tab-panels>
+    </q-card>
+  </div>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
+import CriteriosPage from './CriteriosPage.vue';
+
+const criterio = 0;
+const example = ['Ambiente 1', 'Ambiente 2'];
 
 export default defineComponent({
   name: 'HomePage',
+  components: {
+    CriteriosPage,
+  },
   data() {
     return {
-      totalCriteriosCompletos: 15,
+      tab: 'geral',
+      totalCriteriosCompletos: criterio,
+      ambientes: example,
     };
   },
-  methods: {
-    goToCriterios() {
-      window.location.href = '/#/criterios';
-    },
-  },
   computed: {
-    calcularProgressoTotal: () => Math.floor((15 / 21) * 100),
+    calcularProgressoTotal: () => Math.floor((criterio / 21) * 100),
   },
 });
 </script>
-
-<style>
-.fab {
-  position: absolute;
-  bottom: 16px;
-  right: 16px;
-}
-</style>
