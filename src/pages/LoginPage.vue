216f792c2f7134ac888184ea201facbef8fa11ba
Edison Matias <devedisonmatias@gmail.com>
2022-12-20 15:54:26 -0300

    add admin restriction

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -8,13 +8,15 @@
       <q-card-section>
         <q-form>
           <q-input outlined v-model="email" :error="emailError" no-error-icon type="email" label="E-mail" />
-          <q-input outlined v-model="password" :error="passError" no-error-icon :type="isVisible ? 'text' : 'password'"
-            label="Senha">
-            <template v-slot:append>
-              <q-btn dense rounded flat @click="isVisible = !isVisible"
-                :icon="isVisible ? 'visibility_off' : 'visibility'" />
-            </template>
-          </q-input>
+          <div @keydown.enter="handleLogin()">
+            <q-input outlined v-model="password" :error="passError" no-error-icon
+              :type="isVisible ? 'text' : 'password'" label="Senha">
+              <template v-slot:append>
+                <q-btn dense rounded flat @click="isVisible = !isVisible"
+                  :icon="isVisible ? 'visibility_off' : 'visibility'" />
+              </template>
+            </q-input>
+          </div>
         </q-form>
       </q-card-section>
 
@@ -39,6 +41,7 @@
 </template>
 
 <script>
+import VueJwtDecode from 'vue-jwt-decode';
 import { defineComponent } from 'vue';
 import { api } from '../boot/axios';
 
@@ -72,7 +75,13 @@ export default defineComponent({
 
       if (data.status === 200) {
         localStorage.setItem('apo@session', data.body);
-        window.location.href = '/#/unidade';
+
+        const { cargo } = VueJwtDecode.decode(data.body);
+
+        const ADMIN = 'Administrador do site ou Equipe de TI';
+        const nextPage = cargo === ADMIN ? 'admin' : 'unidade';
+
+        this.$router.push(nextPage);
       } else {
         this.errorMessage = data.body;
       }
