ae0785ea018d554258d03de2c35eba0f0eb432bd
Edison Matias <devedisonmatias@gmail.com>
2023-01-07 22:31:15 -0300

    added criterio 3

---
+++ src/pages/criterios/Acessibilidade.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="q-ma-lg">
+    <div class="q-mb-lg">
+      <div class="text-h6">{{ titulo }}</div>
+      Responda o questionário abaixo
+    </div>
+
+    <q-card>
+      <div v-bind:key="index" v-for="(criterio, index) in criterios">
+        <q-card-section>
+          <div class="text-bold">{{ criterio.toUpperCase() }}</div>
+        </q-card-section>
+
+        <q-card-section v-bind:key="index"
+          v-for="(pergunta, index) in perguntas.filter((item) => item.group === criterio)">
+          <QuestionarioComponent :label="pergunta.label" :type="pergunta.type" :hint="pergunta.hint" />
+
+        </q-card-section>
+      </div>
+
+      <q-card-actions align="right">
+        <q-btn flat label="Concluir" color="primary" />
+      </q-card-actions>
+    </q-card>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+import perguntas from '../../data/form/criterio-3.json';
+import QuestionarioComponent from '../../components/Questionario.vue';
+
+const titulo = 'ACESSIBILIDADE E CIRCULAÇÃO';
+
+const criterios = [
+  'Acessos e circulação no interior da edificação',
+  'Acessibilidade',
+  'Acesso à remoção de resíduos',
+  'Sinalização interna e externa',
+];
+
+export default defineComponent({
+  name: 'DocumentacaoPage',
+  data() {
+    return {
+      titulo,
+      criterios,
+      perguntas,
+    };
+  },
+  components: {
+    QuestionarioComponent,
+  },
+});
+</script>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -5,14 +5,42 @@ const routes: RouteRecordRaw[] = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') },
-      { path: '/home', component: () => import('pages/HomePage.vue') },
-      { path: '/login', component: () => import('pages/LoginPage.vue') },
-      { path: '/signin', component: () => import('pages/SignInPage.vue') },
-      { path: '/admin', component: () => import('pages/AdminPage.vue') },
-      { path: '/unidade', component: () => import('pages/UnidadePage.vue') },
-      { path: '/ambiente', component: () => import('pages/AmbientePage.vue') },
-      { path: '/ambiente/editar', component: () => import('pages/EditarAmbientePage.vue') },
+      {
+        path: '',
+        component: () => import('pages/IndexPage.vue'),
+      },
+      {
+        path: '/home',
+        component: () => import('pages/HomePage.vue'),
+      },
+      {
+        path: '/login',
+        component: () => import('pages/LoginPage.vue'),
+      },
+      {
+        path: '/signin',
+        component: () => import('pages/SignInPage.vue'),
+      },
+      {
+        path: '/admin',
+        component: () => import('pages/AdminPage.vue'),
+      },
+      {
+        path: '/unidade',
+        component: () => import('pages/UnidadePage.vue'),
+      },
+      {
+        path: '/ambiente',
+        component: () => import('pages/AmbientePage.vue'),
+      },
+      {
+        path: '/ambiente/editar',
+        component: () => import('pages/EditarAmbientePage.vue'),
+      },
+      {
+        path: '/criterio/3',
+        component: () => import('src/pages/criterios/Acessibilidade.vue'),
+      },
     ],
   },
 
