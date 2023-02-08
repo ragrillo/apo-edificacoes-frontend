37f6659a1f94ca8738c8ec282a04a53b73b72210
Edison Matias <devedisonmatias@gmail.com>
2023-02-08 15:26:30 -0300

    feat: added api-key

--- src/boot/axios.ts
+++ src/boot/axios.ts
@@ -13,7 +13,10 @@ declare module '@vue/runtime-core' {
 // good idea to move this instance creation inside of the
 // "export default () => {}" function below (which runs individually
 // for each client)
-const api = axios.create({ baseURL: process.env.API_URL });
+const api = axios.create({
+  baseURL: process.env.API_URL,
+  headers: { 'x-api-key': 'apo-api-key' }
+});
 
 export default boot(({ app }) => {
   // for use inside Vue files (Options API) through this.$axios and this.$api
