1ca8008861f6dccd481a6fa8b9522ff5e5397bea
Edison Matias <devedisonmatias@gmail.com>
2023-03-09 07:08:11 -0300

    update: perfil page

--- src/pages/PerfilPage.vue
+++ src/pages/PerfilPage.vue
@@ -1,76 +1,77 @@
 <template>
-  <q-layout view="hHh lpR fFf">
-
-    <q-header elevated class="bg-primary text-white teste">
-      <q-toolbar>
-        <q-toolbar-title class="'justify-center'">
-          Nome da Empresa
-        </q-toolbar-title>
-      </q-toolbar>
-    </q-header>
-
-    <q-page-container>
-
-      <q-card class="my-card">
-        <q-card-section class="titulo-secao">
-          <h1 class="text-h5">Unidades Cadastradas</h1>
-          <hr color="#1976d2">
-        </q-card-section>
-        <CardUnidade v-for="unidade in this.dadosUnidade" :key="unidade.nome" v-bind="unidade" />
-      </q-card>
-      <q-card-section class="column q-gutter-sm">
-        <q-btn unelevated label="Adicionar Unidade" class="btn" @click="cadastrarNovaUnidade" />
+  <q-layout>
+    <q-card class="q-ma-lg">
+      <q-card-section>
+        <div class="text-h6">Unidades Cadastradas</div>
       </q-card-section>
-      <router-view />
-    </q-page-container>
 
+      <q-card-section>
+        <div v-if="possuiUnidades">
+          <q-list v-for="unidade in unidades" :key="unidade.nome">
+            <q-item class="q-py-sm">
+              <q-item-section>
+                <q-item-label>{{ unidade.nome }}</q-item-label>
+                <q-item-label caption>{{ unidade.telefone }}</q-item-label>
+              </q-item-section>
+
+              <q-item-section side>
+                <q-btn flat rounded dense color="primary" icon="more_vert">
+                  <q-menu>
+                    <q-list>
+                      <q-item clickable v-ripple @click="irPara('/unidade')">
+                        <q-item-section>Editar</q-item-section>
+                      </q-item>
+                      <q-item clickable v-ripple @click="irPara('/criterio')">
+                        <q-item-section>Avaliar</q-item-section>
+                      </q-item>
+                    </q-list>
+                  </q-menu>
+                </q-btn>
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </div>
+
+        <span v-else>Não há unidades cadastradas.</span>
+      </q-card-section>
+
+      <q-card-actions align="right">
+        <q-btn flat color="primary" label="Adicionar" to="/unidade" />
+      </q-card-actions>
+    </q-card>
   </q-layout>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
 import VueJwtDecode from 'vue-jwt-decode';
-import CardUnidade from '../components/CardUnidade.vue';
 import { api } from '../boot/axios';
 
-export default defineComponent({
-  name: 'PerfilPage',
+export default {
   data() {
     return {
-      loading: true,
-      dadosUnidade: null,
-      nomeUnidade: null,
-      responsavel: '',
+      unidades: [],
     };
-  },
-  components: {
-    CardUnidade,
   },
   mounted() {
     const token = localStorage.getItem('apo@session');
     this.token = VueJwtDecode.decode(token);
-    this.nomeUnidade = api.get(`unidades/${this.token.edificacao}/proprietario/${this.token.id}`).then((resposta) => { this.dadosUnidade = resposta.data; });
+
+    const { id, edificacao } = this.token;
+    const endpoint = `/unidades/${edificacao}/proprietario/${id}`;
+
+    api.get(endpoint).then(({ data }) => {
+      this.unidades = data;
+    });
   },
   methods: {
-    async cadastrarNovaUnidade() {
-      // console.log(this.token);
-      this.$router.push('unidade');
+    irPara(rota) {
+      this.$router.push(rota);
     },
   },
-
-});
+  computed: {
+    possuiUnidades() {
+      return this.unidades.length > 0;
+    },
+  },
+};
 </script>
-
-<style lang="sass" scoped>
-.teste
-  padding: 10px 15px
-  background: #1976d2
-  border: 1px solid rgba(86,61,124,.2)
-.my-card
-  width:100%
-.card-unidade
-  background: #ecf2ff
-  margin-bottom: 10px
-.btn
-  background: #ffc107
-</style>
