1f9fa38c898b934e34e7f55553054f24a9ff0ff8
Edison Matias <devedisonmatias@gmail.com>
2023-03-16 10:59:16 -0300

    update (criterios): ensure that each question has one single answer

--- src/pages/CriterioPage.vue
+++ src/pages/CriterioPage.vue
@@ -7,6 +7,7 @@
     </q-card-section>
 
     <q-card-actions align="right">
+      <q-btn flat color="primary" label="Voltar" @click="$router.back()" />
       <q-btn flat color="primary" label="Enviar" @click="enviarRespostas" />
     </q-card-actions>
   </q-card>
@@ -31,10 +32,17 @@ export default ({
     this.getCriterios(numero);
   },
   methods: {
-    registrarResposta(criterio) {
-      this.respostas.push(criterio);
+    registrarResposta(data) {
+      const index = this.respostas.findIndex((item) => item.criterio === data.criterio);
+
+      if (index === -1) {
+        this.respostas.push(data);
+      } else {
+        this.respostas[index] = data;
+      }
     },
     async enviarRespostas() {
+      console.log(this.respostas);
       const { ambienteid } = this.$route.params;
 
       const payload = {
