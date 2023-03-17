80d7fbb7fd9dbc13b541384addf3ff7703936b93
Gustavo Muller Keiser <gustavokeiser@outlook.com>
2023-03-17 15:40:34 -0300

    update perfil

--- src/pages/PerfilPage.vue
+++ src/pages/PerfilPage.vue
@@ -4,8 +4,16 @@
       <q-card-section>
         <div class="text-h6">Unidades Cadastradas</div>
       </q-card-section>
-
-      <q-card-section>
+      <div v-if="visible">
+        <q-inner-loading
+          :showing="visible"
+          color="primary"
+          label="Aguarde..."
+          label-class="text-primary"
+          label-style="font-size: 1.1em"
+        />
+      </div>
+      <q-card-section v-else>
         <div v-if="possuiUnidades">
           <q-list v-for="unidade in unidades" :key="unidade.nome">
             <q-item>
@@ -15,18 +23,30 @@
                   <q-item-label caption>{{ unidade.telefone }}</q-item-label>
                 </div>
                 <div>
-                  <q-btn flat dense color="primary" label="Iniciar avaliação" @click="irPara(`/unidade/${unidade._id}/ambiente`)"/>
+                  <q-btn
+                    flat
+                    dense
+                    color="primary"
+                    label="Iniciar avaliação"
+                    @click="irPara(`/unidade/${unidade._id}/ambiente`)"
+                  />
                 </div>
               </q-item-section>
             </q-item>
-            <q-separator class="q-mb-sm"/>
+            <q-separator class="q-mb-sm" />
           </q-list>
         </div>
 
         <span v-else>Não há unidades cadastradas.</span>
       </q-card-section>
-
       <q-card-actions align="right">
+        <q-btn
+          flat
+          round
+          color="primary"
+          icon="refresh"
+          @click="obterUnidades()"
+        />
         <q-btn flat color="primary" label="Adicionar" to="/unidade" />
       </q-card-actions>
     </q-card>
@@ -39,6 +59,7 @@ import { api } from '../boot/axios';
 export default {
   data() {
     return {
+      visible: true,
       unidades: [],
     };
   },
@@ -57,6 +78,7 @@ export default {
       this.obterUnidades();
     },
     async obterUnidades() {
+      this.visible = true;
       const id = localStorage.getItem('apo@usuario_id');
       const edificacao = localStorage.getItem('apo@usuario_edificacao');
 
@@ -64,6 +86,7 @@ export default {
       const { data } = await api.get(endpoint);
 
       this.unidades = data;
+      this.visible = false;
     },
   },
   computed: {
