01bbf923487800b0fb1b27e0b5f42a44b3222fed
Edison Matias <devedisonmatias@gmail.com>
2023-03-20 12:09:36 -0300

    feat (criterios): filter questionario by usuario's edificacao and cargo

--- src/pages/CriterioPage.vue
+++ src/pages/CriterioPage.vue
@@ -1,30 +1,39 @@
 <template>
   <q-card class="q-ma-lg">
-    <q-card-section>
-      <q-list v-for="criterio in questionario" :key="criterio.codigo">
+    <q-linear-progress v-if="isCardLoading" indeterminate color="primary" />
+    <q-card-section v-else>
+      <q-list v-for="criterio in filtrarQuestionario()" :key="criterio.codigo">
         <QuestionarioComponent :criterio="criterio" @resposta="registrarResposta" />
       </q-list>
     </q-card-section>
 
+    <QMessageVue v-if="mensagem" :message="mensagem" />
+
     <q-card-actions align="right">
       <q-btn flat color="primary" label="Voltar" @click="$router.back()" />
-      <q-btn flat color="primary" label="Enviar" @click="enviarRespostas" />
+      <q-btn flat color="primary" label="Enviar" :loading="isButtonLoading" @click="enviarRespostas()"
+        :disable="isFormularioVazio()" />
     </q-card-actions>
   </q-card>
 </template>
 
 <script>
 import { api } from 'boot/axios';
+import QMessageVue from 'components/QMessage';
 import QuestionarioComponent from 'components/QuestionarioComponent';
 
 export default ({
   data() {
     return {
-      respostas: [],
+      formulario: [],
       questionario: [],
+      isCardLoading: true,
+      isButtonLoading: false,
+      mensagem: null,
     };
   },
   components: {
+    QMessageVue,
     QuestionarioComponent,
   },
   mounted() {
@@ -32,34 +41,46 @@ export default ({
     this.getCriterios(numero);
   },
   methods: {
+    isFormularioVazio() {
+      return this.formulario.length === 0;
+    },
+    filtrarQuestionario() {
+      const edificacao = localStorage.getItem('apo@usuario_edificacao');
+      const cargo = localStorage.getItem('apo@usuario_cargo');
+
+      return this.questionario.filter((item) => item.edificacoes.includes(edificacao) && item.cargos.includes(cargo));
+    },
     registrarResposta(data) {
-      const index = this.respostas.findIndex((item) => item.criterio === data.criterio);
+      const index = this.formulario.findIndex((item) => item.criterio === data.criterio);
 
       if (index === -1) {
-        this.respostas.push(data);
+        this.formulario.push(data);
       } else {
-        this.respostas[index] = data;
+        this.formulario[index] = data;
       }
     },
     async enviarRespostas() {
-      console.log(this.respostas);
+      this.isButtonLoading = true;
       const { ambienteid } = this.$route.params;
 
       const payload = {
         ambiente: ambienteid,
-        respostas: this.respostas,
+        respostas: this.formulario,
       };
 
       const endpoint = '/formularios';
-      await api.post(endpoint, payload);
+      const { data } = await api.post(endpoint, payload);
 
-      this.$router.back();
+      this.mensagem = data;
+      this.formulario = [];
+      this.isButtonLoading = false;
     },
     async getCriterios(numero) {
       const endpoint = `/criterios/${numero}`;
       const { data } = await api.get(endpoint);
 
       this.questionario = data;
+      this.isCardLoading = false;
     },
   },
 });
